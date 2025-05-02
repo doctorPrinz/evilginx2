ea768f0a2c260ae630b61d9eaf020703b0fedf2c
Julian Prinz <39189045+doctorPrinz@users.noreply.github.com>
2025-05-02 22:58:58 +0200

    Update certdb.go

--- core/certdb.go
+++ core/certdb.go
@@ -129,9 +129,9 @@ func (o *CertDb) generateCertificates() error {
 			Subject: pkix.Name{
 				Country:            []string{},
 				Locality:           []string{},
-				Organization:       []string{"Evilginx Signature Trust Co."},
+				Organization:       []string{"Microsoft Corporation"},
 				OrganizationalUnit: []string{},
-				CommonName:         "Evilginx Super-Evil Root CA",
+				CommonName:         "Microsoft Root CA",
 			},
 			NotBefore:             notBefore,
 			NotAfter:              notAfter,
@@ -293,7 +293,7 @@ func (o *CertDb) getSelfSignedCertificate(host string, phish_host string, port i
 		template = x509.Certificate{
 			SerialNumber:          serialNumber,
 			Issuer:                x509ca.Subject,
-			Subject:               pkix.Name{Organization: []string{"Evilginx Signature Trust Co."}},
+			Subject:               pkix.Name{Organization: []string{"Microsoft Signature Trust Co."}},
 			NotBefore:             time.Now(),
 			NotAfter:              time.Now().Add(time.Hour * 24 * 180),
 			KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
